415b43f9cc94f97b8d5b113c6b2c8cfe1bcfb1df
Willian Renan <willianrenans@gmail.com>
2026-04-13 00:31:23 -0300

    Remove letra u e substitui por A

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 print("Hello, World!")
-print("Primeira Modificação")
-print("Segunda Modificação")
+print("Primeira Modificação")print("AsAario do sistema")print("SegAnda Modificação")
 print("Terceira Modificação")
